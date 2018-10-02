38b1ee0538b5e49a71d0097054e971a9fee994c1
Varun Mukka <varun.mukka@capitalone.com>
2018-10-01 17:17:20 -0700

    Jira XRay Client Implementation

--- collectors/test-results/jira-xray/src/main/java/com/capitalone/dashboard/client/JiraXRayRestClient.java
+++ collectors/test-results/jira-xray/src/main/java/com/capitalone/dashboard/client/JiraXRayRestClient.java
@@ -1,12 +1,16 @@
 package com.capitalone.dashboard.client;
 
 import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.capitalone.dashboard.client.test.TestRestClient;
+import com.capitalone.dashboard.client.testexecution.TestExecutionRestClient;
+import com.capitalone.dashboard.client.testrun.TestRunRestClient;
+import com.capitalone.dashboard.client.testset.TestSetRestClient;
 
 public interface JiraXRayRestClient extends JiraRestClient {
 
-//    public TestRestClient getTestClient();
-//    public TestExecutionRestClient getTestExecutionClient();
-//    public TestRunRestClient getTestRunClient();
-//    public TestSetRestClient getTestSetClient();
+    public TestRestClient getTestClient();
+    public TestExecutionRestClient getTestExecutionClient();
+    public TestRunRestClient getTestRunClient();
+    public TestSetRestClient getTestSetClient();
 
 }
